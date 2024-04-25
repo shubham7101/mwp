c7a9c2aa8b7f065bb1fe3e702e8a9dd5f694a99a
goofw <59505666+goofw@users.noreply.github.com>
2024-04-25 17:20:02 +0800

    wtf

--- src/providers/sources/showbox/sendRequest.ts
+++ src/providers/sources/showbox/sendRequest.ts
@@ -56,6 +56,6 @@ export const sendRequest = async (ctx: ScrapeContext, data: object, altApi = tru
   console.log(data);
   const res = JSON.parse(response);
   //if (JSON.parse(data.toString()).module in [ 'Movie_downloadurl_v3', 'TV_downloadurl_v3' ] || res.code === 0)
-  //  console.log(res);
+    console.log(res);
   return res;
 };
